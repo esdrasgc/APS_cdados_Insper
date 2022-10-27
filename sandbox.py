b3a651bada660e7bfb3044654c26ffeba6d592d5
Esdras Gomes Carvalho <esdrasgcdev@gmail.com>
2022-10-27 00:48:46 -0300

    aps3

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,3 @@
-plt.style.use('default')
-plt.figure(figsize=(16,7))
+from scipy import stats
 
-plt.plot(df_saude.index(), df_saude['Taxa de mortalidade até 5 anos, no Brasil'])
-plt.plot(df_saude.index(), df_saude['Taxa de mortalidade até 5 anos, na Alemanha'])
-plt.plot(df_saude.index() ,df_saude['Taxa de mortalidade até 5 anos, em Moçambique'])
-
-plt.xticks(range(2001, 2021))
-plt.legend(['Brasil', 'Alemanha', 'Moçambique'])
-plt.xlabel('Ano')
-plt.ylabel('% total da população')
-plt.title('Acesso à eletricidade')
-plt.show()
+print(1-stats.binom.cdf(7, 11, 0.5))
